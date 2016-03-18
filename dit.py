afa54a168074f87887fc639ea2b6b29323351ed0
Renat <rzhilkibaev@gmail.com>
2016-03-18 15:57:29 -0700

    Less verbose logging

--- dit.py
+++ dit.py
@@ -40,7 +40,7 @@ def main(args):
     return global_exit_code
 
 def run_test_suite(args, doc, current_suite_index):
-    print_to_console("Running test suite: " + doc["name"])
+    print_to_console(doc["name"])
     
     client = create_docker_client(args["--docker-api-url"])
     
@@ -87,20 +87,20 @@ def start_main_container(main_container, doc, client):
     client.start(main_container)
     ready_message = doc.get("ready_message")
     if ready_message:
-        print_to_console("waiting for ready message " + ready_message)
+#         print_to_console("    waiting for ready message " + ready_message)
         for line in client.logs(main_container, stream=True):
             if ready_message in line.decode("UTF-8"):
                 break
     wait_s = doc.get("wait_s")
     if wait_s:
-        print_to_console("waiting for " + str(wait_s) + " seconds")
+#         print_to_console("    waiting for " + str(wait_s) + " seconds")
         time.sleep(wait_s)
 
 
 def run_test(test, main_container, current_suite_index, current_test_index, client):
     exit_code = -10
     try:
-        print_to_console("Ensuring that " + test["ensures_that"])
+        print_to_console("    " + test["ensures_that"])
         log("Creating test container from image " + test["image"] + " with command " + test.get("command"))
         test_container = client.create_container(test["image"], command=test.get("command"))
         try:
@@ -112,16 +112,16 @@ def run_test(test, main_container, current_suite_index, current_test_index, clie
             exit_code = int(client.wait(test_container, timeout=test_timeout_seconds))
         finally:
             if exit_code != 0:
-                print_to_console("[Fail]")
+                print_to_console("    [Fail]")
                 log("Exit code " + str(exit_code))
             else:
-                print_to_console("[Pass]")
+                print_to_console("    [Pass]")
             log("Saving test container logs " + test_container.get("Id"))
             save_container_logs(test_container, "suite_" + str(current_suite_index) + "_test_" + str(current_test_index) + "_" + test_container.get("Id"), client)
             log("Removing test container " + test_container.get("Id"))
             client.remove_container(test_container, force=True)
     except:
-        print_to_console("Unexpected error " + str(sys.exc_info()))
+        print_to_console("    Unexpected error " + str(sys.exc_info()))
         
     return exit_code
         
