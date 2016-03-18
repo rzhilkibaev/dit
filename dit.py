ffaeeb4add9daa154379c28d2be2f42b5c9b3d6a
Renat <rzhilkibaev@gmail.com>
2016-03-17 22:43:19 -0700

    Somewhat working

--- dit.py
+++ dit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import yaml
 import docker
+import yaml
+
 
 def main():
     with open("test.yml", "r") as f:
@@ -12,12 +13,35 @@ def main():
     client = docker.Client(base_url="unix://var/run/docker.sock")
     container = client.create_container(image=doc["image"])
     try:
-        print("Created container " + str(container))
+        print("Created container " + str(container["Id"]))
+        print("Testing that:")
+        for test in doc["tests"]:
+            run_test(test, client)
         # run tests
     finally:
-        # remove mage
+        # remove image
         client.remove_container(container, force=True)
     
-    
+
+def run_test(test, client):
+    try:
+        print(test["ensures_that"])
+        container = client.create_container(image=test["image"], command=test.get("command"))
+        try:
+            client.start(container)
+            exit_code = client.wait(container, timeout=_test_timeout_seconds)
+            if exit_code != 0:
+                print("[Fail]")
+            else:
+                print("[Pass]")
+        finally:
+#             print(str(client.logs(container)))
+            client.remove_container(container, force=True)
+        
+    except:
+        print("Unexpected error")
+
+_test_timeout_seconds=60*10
+
 if __name__ == "__main__":
     main()
