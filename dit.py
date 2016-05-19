1333171bb158765f5a0f7fb78f65121fbbc63736
Renat <rzhilkibaev@gmail.com>
2016-05-19 12:48:10 -0700

    Save some work

--- dit.py
+++ dit.py
@@ -19,50 +19,53 @@ import traceback
 
 _default_test_timeout_seconds = 60 * 1
 _execution_id = time.strftime("%Y-%m-%d_%H-%M-%S")
-_test_results_directory = "test-results/" + _execution_id
+_test_results_directory = "dit/" + _execution_id
 
 
 def main(args):
-    global_exit_code = 0
-    file_to_load = args["--file"] if args["--file"] else "dit.yml"
-    log("Loading " + file_to_load)
+    dit_config = load_dit_config(nvl(args["--file"], "dit.yml"))
+    docker_client = create_docker_client(nvl(args["--docker-api-url"], "unix://var/run/docker.sock"))
+    exit_code = 0
     current_suite_index = 1
-    with open(file_to_load) as f:
-        for doc in yaml.load_all(f):
-            suite_exit_code = run_test_suite(args, doc, current_suite_index)
-            if suite_exit_code:
-                global_exit_code = 1
-            current_suite_index += 1
-        if global_exit_code == 0:
-            print_to_console("[SUCCESS]")
-        else:
-            print_to_console("[FAILURE]")
-    return global_exit_code
-
-def run_test_suite(args, doc, current_suite_index):
-    print_to_console(doc["name"])
-    
-    client = create_docker_client(args["--docker-api-url"])
-    
-    log("Creating main container from image " + doc["image"])
-    main_container = client.create_container(doc["image"], environment=doc.get("env"))
-    suite_exit_code = 0
-    try:
-        start_main_container(main_container, doc, client)
-
-        current_test_index = 1
+    for suite_config in dit_config["suites"]:
+        suite_exit_code = run_suite(current_suite_index, docker_client, dit_config["image"], suite_config)
+        current_suite_index += 1
+        if suite_exit_code != 0:
+            exit_code = 1
         
-        for test in doc["tests"]:
-            log("test " + str(current_test_index))
-            test_exit_code = run_test(test, main_container, current_suite_index, current_test_index, client)
-            if test_exit_code != 0:
-                suite_exit_code = 1
-            current_test_index += 1
-    finally:
-        log("Saving main container logs " + main_container.get("Id"))
-        save_container_logs(main_container, "suite_" + str(current_suite_index) + "_" + main_container.get("Id"), client)
-        log("Removing main container " + main_container.get("Id"))
-        client.remove_container(main_container, force=True)
+    print_to_console("[SUCCESS]" if exit_code == 0 else "[FAILURE]")
+    return exit_code
+
+def load_dit_config(dit_yml_location):
+    log("Loading " + dit_yml_location)
+    with open(dit_yml_location) as f:
+        return yaml.load(f)
+    
+
+def run_suite(current_suite_index, docker_client, image, suite_config):
+    try:
+        print_to_console(suite_config["name"])
+        
+        log("Creating main container from image " + image)
+        main_container = docker_client.create_container(image, environment=suite_config.get("env"))
+        try:
+            suite_exit_code = 0
+            start_main_container(main_container, suite_config, client)
+            current_test_index = 1
+            for test_config in suite_config["tests"]:
+                log("test " + str(current_test_index))
+                test_exit_code = run_test(test_config, main_container, current_suite_index, current_test_index, client)
+                if test_exit_code != 0:
+                    suite_exit_code = 1
+                current_test_index += 1
+        finally:
+            log("Saving main container logs " + main_container.get("Id"))
+            save_container_logs(main_container, "suite_" + str(current_suite_index) + "_" + main_container.get("Id"), client)
+            log("Removing main container " + main_container.get("Id"))
+            client.remove_container(main_container, force=True)
+    except:
+        traceback.print_exc()
+        suite_exit_code = 1
         
     return suite_exit_code
 
@@ -77,9 +80,8 @@ def load_test_file(file):
         
 
 def create_docker_client(docker_api_url):
-    url = docker_api_url if docker_api_url else "unix://var/run/docker.sock"
-    log("Creating docker client with docker api url " + url)
-    return docker.Client(base_url=url)
+    log("Creating docker client with docker api url " + docker_api_url)
+    return docker.Client(base_url=docker_api_url)
 
 
 def start_main_container(main_container, doc, client):
@@ -97,17 +99,17 @@ def start_main_container(main_container, doc, client):
         time.sleep(wait_s)
 
 
-def run_test(test, main_container, current_suite_index, current_test_index, client):
+def run_test(test_config, main_container, current_suite_index, current_test_index, client):
     exit_code = -10
     try:
-        print_to_console("    " + test["ensures_that"])
-        log("Creating test container from image " + test["image"] + " with command " + test.get("command"))
-        test_container = client.create_container(test["image"], command=test.get("command"))
+        print_to_console("    " + test_config["ensures_that"])
+        log("Creating test container from image " + test_config["image"] + " with command " + test_config.get("command"))
+        test_container = client.create_container(test_config["image"], command=test_config.get("command"))
         try:
             log("Starting test container " + test_container.get("Id"))
             client.start(test_container, links={main_container["Id"]:"main"})
             log("Waiting for test container " + test_container.get("Id"))
-            test_timeout_seconds = test.get("timeout_s")
+            test_timeout_seconds = test_config.get("timeout_s")
             test_timeout_seconds = test_timeout_seconds if test_timeout_seconds else _default_test_timeout_seconds
             exit_code = int(client.wait(test_container, timeout=test_timeout_seconds))
         finally:
@@ -144,6 +146,9 @@ def log(msg):
     os.makedirs(_test_results_directory, exist_ok=True)
     with open(_test_results_directory + "/dit.log", "a") as f:
         f.write(msg + "\n")
+        
+def nvl(value, default):
+    return value if value else default
 
 
 if __name__ == "__main__":
